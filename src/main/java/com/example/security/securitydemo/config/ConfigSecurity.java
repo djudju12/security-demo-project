d9a924be768f1b3748651b91f12c4b0a955fbb4e
djudju12 <jonathan.willian321@gmail.com>
2023-07-12 11:17:27 -0300

    cors e csrf

--- src/main/java/com/example/security/securitydemo/config/ConfigSecurity.java
+++ src/main/java/com/example/security/securitydemo/config/ConfigSecurity.java
@@ -1,12 +1,20 @@
 package com.example.security.securitydemo.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,14 +23,43 @@ public class ConfigSecurity {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestAttributeHandler = new CsrfTokenRequestAttributeHandler();
+        requestAttributeHandler.setCsrfRequestAttributeName("_csrf");
+
         http
+                .securityContext(context -> context
+                        .requireExplicitSave(false))
+
+                .sessionManagement(session ->   session
+                        .sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+
+                .cors(cors -> cors
+                        .configurationSource(new CorsConfigurationSource() {
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                                CorsConfiguration config = new CorsConfiguration();
+                                config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                                config.setAllowedMethods(Collections.singletonList("*"));
+                                config.setAllowCredentials(true);
+                                config.setAllowedHeaders(Collections.singletonList("*"));
+                                config.setMaxAge(3600L);
+                                return config;
+                            }
+                        }))
+
+                .csrf(csrf -> csrf
+                        .csrfTokenRequestHandler(requestAttributeHandler)
+                            .ignoringRequestMatchers("/contact", "/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contacts", "/register", "/users").permitAll()
+//                            .hasAnyAuthority("VIEWCONTACTS")
                 )
                 .formLogin(withDefaults())
-                .httpBasic(withDefaults())
-                .csrf().disable();
+                .httpBasic(withDefaults());
 
         return http.build();
     }
